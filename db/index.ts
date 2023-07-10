da0d96d1cf948e0e9b31a710a10606257332d405
zhangmengnan <823421131@qq.com>
2023-07-10 17:27:25 +0800

    fix: environmental distinction

--- db/index.ts
+++ db/index.ts
@@ -1,42 +1,40 @@
-import 'reflect-metadata';
-import { Connection, createConnection, getConnection } from 'typeorm';
-import { User, UserAuth } from './entity/index';
+import "reflect-metadata";
+import { Connection, createConnection, getConnection } from "typeorm";
+import { User, UserAuth } from "./entity/index";
 
 let connectionReadyPromise: Promise<Connection> | null = null;
 const {
-  DATABASE_HOST,
-  DATABASE_PORT,
-  DATABASE_USERNAME,
-  DATABASE_PASSWORD,
-  DATABASE_NAME,
+    DATABASE_HOST,
+    DATABASE_PORT,
+    DATABASE_USERNAME,
+    DATABASE_PASSWORD,
+    DATABASE_NAME,
 } = process.env;
 const entities = [User, UserAuth];
 
 export const prepareConnection = () => {
-  if (!connectionReadyPromise) {
-    connectionReadyPromise = (async () => {
-      try {
-        const staleConnection = getConnection();
-        await staleConnection.close();
-      } catch (error) {
-        console.log(error);
-      }
+    if (!connectionReadyPromise) {
+        connectionReadyPromise = (async () => {
+            try {
+                const staleConnection = getConnection();
+                await staleConnection.close();
+            } catch (error) {
+                console.log(error);
+            }
 
-      const connection = await createConnection({
-        type: 'mysql',
-        host: DATABASE_HOST,
-        port: Number(DATABASE_PORT),
-        username: DATABASE_USERNAME,
-        password: DATABASE_PASSWORD,
-        database: DATABASE_NAME,
-        entities,
-        synchronize: false,
-        logging: true,
-      });
+            return await createConnection({
+                type: "mysql",
+                host: DATABASE_HOST,
+                port: Number(DATABASE_PORT),
+                username: DATABASE_USERNAME,
+                password: DATABASE_PASSWORD,
+                database: DATABASE_NAME,
+                entities,
+                synchronize: false,
+                logging: true,
+            });
+        })();
+    }
 
-      return connection;
-    })();
-  }
-
-  return connectionReadyPromise;
+    return connectionReadyPromise;
 };
--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -12,86 +12,88 @@ import { ISession } from "@/pages/api/types";
 async function login(req: NextApiRequest, res: NextApiResponse) {
     const { session } = req as { session: any };
     const cookies = Cookie.fromApiRoute(req, res);
-    // const { phone = '', verify = '', identity_type = 'phone' } = req.body;
-    // const db = await prepareConnection();
-    // const userAuthRepo = db.getRepository(UserAuth);
-    const userInfo: Pick<User, "id" | "nickname" | "avatar"> = {
+    const { phone = "", verify = "", identity_type = "phone" } = req.body;
+    const db = await prepareConnection();
+    const userAuthRepo = db.getRepository(UserAuth);
+    let userInfo: Pick<User, "id" | "nickname" | "avatar"> = {
         id: 1,
         nickname: "nnn",
         avatar: "",
     };
 
-    session.userId = userInfo.id;
-    session.nickname = userInfo.nickname;
-    session.avatar = userInfo.avatar;
+    if (process.env.NODE_ENV === "development") {
+        session.userId = userInfo.id;
+        session.nickname = userInfo.nickname;
+        session.avatar = userInfo.avatar;
 
-    await session.save();
+        await session.save();
 
-    setCookie(cookies, {
-        id: userInfo.id,
-        nickname: userInfo.nickname,
-        avatar: userInfo.avatar,
-    });
-
-    res?.status(200).json({
-        code: 0,
-        msg: "登录成功",
-        data: {
+        setCookie(cookies, {
             id: userInfo.id,
             nickname: userInfo.nickname,
             avatar: userInfo.avatar,
-        },
-    });
+        });
 
-    // if (String(session.verifyCode) === String(verify)) {
-    //   // 验证码正确，在 user_auths 表中查找 identity_type 是否有记录
-    //   const userAuth = await userAuthRepo.findOne(
-    //     { identity_type, identifier: phone },
-    //     { relations: ['user'] }
-    //   );
+        res?.status(200).json({
+            code: 0,
+            msg: "登录成功",
+            data: {
+                id: userInfo.id,
+                nickname: userInfo.nickname,
+                avatar: userInfo.avatar,
+            },
+        });
+    }
 
-    //   // 已存在的用户
-    //   if (userAuth) userInfo = userAuth.user;
-    //   else {
-    //     // 新用户，自动注册
-    //     const user = new User();
-    //     user.nickname = `用户_${Math.floor(Math.random() * 10000)}`;
-    //     user.avatar = '/images/avatar.jpg';
-    //     user.job = '暂无';
-    //     user.introduce = '暂无';
+    if (String(session.verifyCode) === String(verify)) {
+        // 验证码正确，在 user_auths 表中查找 identity_type 是否有记录
+        const userAuth = await userAuthRepo.findOne(
+            { identity_type, identifier: phone },
+            { relations: ["user"] }
+        );
 
-    //     const userAuth = new UserAuth();
-    //     userAuth.identifier = phone;
-    //     userAuth.identity_type = identity_type;
-    //     userAuth.credential = session.verifyCode;
-    //     userAuth.user = user;
+        // 已存在的用户
+        if (userAuth) userInfo = userAuth.user;
+        else {
+            // 新用户，自动注册
+            const user = new User();
+            user.nickname = `用户_${Math.floor(Math.random() * 10000)}`;
+            user.avatar = "/images/avatar.jpg";
+            user.job = "暂无";
+            user.introduce = "暂无";
 
-    //     const { user: resUserAuth } = await userAuthRepo.save(userAuth);
-    //     userInfo = resUserAuth;
-    //   }
+            const userAuthData = new UserAuth();
+            userAuthData.identifier = phone;
+            userAuthData.identity_type = identity_type;
+            userAuthData.credential = session.verifyCode;
+            userAuthData.user = user;
 
-    //   session.userId = userInfo.id;
-    //   session.nickname = userInfo.nickname;
-    //   session.avatar = userInfo.avatar;
+            const { user: resUserAuth } = await userAuthRepo.save(userAuthData);
+            userInfo = resUserAuth;
+        }
 
-    //   await session.save();
+        session.userId = userInfo.id;
+        session.nickname = userInfo.nickname;
+        session.avatar = userInfo.avatar;
 
-    //   setCookie(cookies, {
-    //     id: userInfo.id,
-    //     nickname: userInfo.nickname,
-    //     avatar: userInfo.avatar,
-    //   });
+        await session.save();
 
-    //   res?.status(200).json({
-    //     code: 0,
-    //     msg: '登录成功',
-    //     data: {
-    //       id: userInfo.id,
-    //       nickname: userInfo.nickname,
-    //       avatar: userInfo.avatar,
-    //     },
-    //   });
-    // } else res?.status(200).json({ code: -1, msg: '验证码错误' });
+        setCookie(cookies, {
+            id: userInfo.id,
+            nickname: userInfo.nickname,
+            avatar: userInfo.avatar,
+        });
+
+        res?.status(200).json({
+            code: 0,
+            msg: "登录成功",
+            data: {
+                id: userInfo.id,
+                nickname: userInfo.nickname,
+                avatar: userInfo.avatar,
+            },
+        });
+    } else res?.status(200).json({ code: -1, msg: "验证码错误" });
 }
 
 export default withIronSessionApiRoute(login, ironOptions);
